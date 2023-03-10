1b6c09ac5a44fcdd8c1cb071ca9f8f5d57f649c1
LMSecurity <lm.security@lianmedia.ru>
2023-03-10 14:49:16 +0500

    fix mistake

--- tools/ip_tools.py
+++ tools/ip_tools.py
@@ -10,7 +10,7 @@ def get_self_ip(iface: str):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         ip = socket.inet_ntoa(fcntl.ioctl(s.fileno(),0x8915,struct.pack('256s', iface.encode()[:15]))[20:24])
-        mac = ':'.join('%02x' % b for b in fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', bytes('enp1s0', 'utf-8')[:15]))[18:24])
+        mac = ':'.join('%02x' % b for b in fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', bytes(iface, 'utf-8')[:15]))[18:24])
         return {'ip': ip, 'mac': mac}
     except:
         raise Exception('Can not get IP address for iface %s' % iface)
